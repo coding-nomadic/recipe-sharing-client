4b6d23da40efaad744264eff9248f9e17d5d1927
gyurmey <gyurmey1835@gmail.com>
2023-06-03 16:36:31 +0200

    add image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import grill from "./images/grill.jpeg";
 
 function App()
 {
   return (
     <div className="App">
-      Hello
+      <h2>
+        <i>
+          Recipe Sharing
+        </i>
+      </h2>
+      <img src={ grill } alt="grill" style={ { borderRadius: "20px" } } />
     </div>
   );
 }
