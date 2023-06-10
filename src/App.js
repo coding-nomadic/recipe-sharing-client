bc6d4ba21a333928c29f8829e71d9b22b99cef1b
gyurmey <gyurmey1835@gmail.com>
2023-06-10 14:15:38 +0200

    getr api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 import grill from "./images/grill.jpeg";
+import axios from "axios";
+import React, { useEffect } from "react";
 
 function App()
 {
+  useEffect(async () =>
+  {
+    const baseURL = "https://recipe-app-service.herokuapp.com/api/v1/recipes";
+    try
+    {
+      let responseData = await axios.get(`${baseURL}`).then((res) =>
+      {
+        console.log('response', res);
+      })
+      console.log("response ", responseData)
+
+    } catch (e)
+    {
+      console.log(e)
+    }
+  }, [])
+
   return (
     <div className="App">
       <h1>
