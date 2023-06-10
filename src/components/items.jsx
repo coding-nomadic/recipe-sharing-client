b5f0a6c46cb2fcc811f401ea50fb295209d985cb
gyurmey <gyurmey1835@gmail.com>
2023-06-10 21:47:57 +0200

    key

--- src/components/items.jsx
+++ src/components/items.jsx
@@ -4,8 +4,8 @@ import Item from './Item'
 function Items({data}) {
   return (
     <div>
-      {!!data && data.map(el => {
-       return <Item data={el} />
+      {!!data && data.map((el, i) => {
+       return <Item key={i} data={el} />
       })}</div>
   )
 }
