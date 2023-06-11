0b99a75b7dca33bd12bf08a92d693e5b94639e7d
gyurmey <gyurmey1835@gmail.com>
2023-06-11 14:55:34 +0200

    classname

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 function header() {
 
     return (
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div style={{marginLeft: "40vw" }}>
-        <a class="navbar-brand" style={{fontFamily: "initial",color: "red"}} href="#">Category</a>
-        <a class="navbar-brand" style={{fontFamily: "initial",color: "blueviolet"}}  href="#">Items</a> 
-        <a class="navbar-brand" style={{fontFamily: "initial",color: "greenyellow"}}  href="#">Recipe</a>
-        <a class="navbar-brand" style={{fontFamily: "initial",color: "saddlebrown"}}  href="#">Check</a>
+        <a className="navbar-brand" style={{fontFamily: "initial",color: "red"}} href="#">Category</a>
+        <a className="navbar-brand" style={{fontFamily: "initial",color: "blueviolet"}}  href="#">Items</a> 
+        <a className="navbar-brand" style={{fontFamily: "initial",color: "greenyellow"}}  href="#">Recipe</a>
+        <a className="navbar-brand" style={{fontFamily: "initial",color: "saddlebrown"}}  href="#">Check</a>
       </div>
 </nav>
 
