671ecf08faa00cbf5b8b661158e56ddb3768190d
gyurmey <gyurmey1835@gmail.com>
2023-06-17 14:31:44 +0200

    narv bar

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,13 +5,22 @@ function header() {
     return (
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div style={{marginLeft: "40vw" }}>
-        <a className="navbar-brand" style={{fontFamily: "initial"}} href="#">Category</a>
-        <a className="navbar-brand" style={{fontFamily: "initial"}}  href="#">Items</a> 
-        <a className="navbar-brand" style={{fontFamily: "initial"}}  href="#">Recipe</a>
-        <a className="navbar-brand" style={{fontFamily: "initial"}}  href="#">Check</a>
-      </div>
-</nav>
 
+
+        <a className="navbar-brand" style={{fontFamily: "initial", marginRight: "150px",marginLeft: "-300px"}} href="#">Recipe Sharing</a>
+
+          <a className="navbar-brand" style={{fontFamily: "initial"}} href="#">Category</a>
+          <a className="navbar-brand" style={{fontFamily: "initial"}}  href="#">Items</a> 
+          <a className="navbar-brand" style={{fontFamily: "initial"}}  href="#">Recipe</a>
+          <a className="navbar-brand" style={{fontFamily: "initial"}}  href="#">Check</a>
+
+
+
+          <a className="navbar-brand" style={{fontFamily: "initial", marginLeft: "150px",marginRight: "-300px"}}  href="#">Sign In</a>
+
+
+      </div>
+    </nav>
 
 
   )
