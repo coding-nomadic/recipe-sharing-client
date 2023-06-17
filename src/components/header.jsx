0696467656007484e3e6e2d25c1cef59cdbd4072
gyurmey <gyurmey1835@gmail.com>
2023-06-17 14:26:37 +0200

    color

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,10 +5,10 @@ function header() {
     return (
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div style={{marginLeft: "40vw" }}>
-        <a className="navbar-brand" style={{fontFamily: "initial",color: "red"}} href="#">Category</a>
-        <a className="navbar-brand" style={{fontFamily: "initial",color: "blueviolet"}}  href="#">Items</a> 
-        <a className="navbar-brand" style={{fontFamily: "initial",color: "greenyellow"}}  href="#">Recipe</a>
-        <a className="navbar-brand" style={{fontFamily: "initial",color: "saddlebrown"}}  href="#">Check</a>
+        <a className="navbar-brand" style={{fontFamily: "initial"}} href="#">Category</a>
+        <a className="navbar-brand" style={{fontFamily: "initial"}}  href="#">Items</a> 
+        <a className="navbar-brand" style={{fontFamily: "initial"}}  href="#">Recipe</a>
+        <a className="navbar-brand" style={{fontFamily: "initial"}}  href="#">Check</a>
       </div>
 </nav>
 
