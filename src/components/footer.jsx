8452dafafa28313e20ce4e6b17d31a75c9d65c2f
gyurmey <gyurmey1835@gmail.com>
2023-06-17 17:44:24 +0200

    Footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { BsInstagram } from 'react-icons/bs';
+import { GrFacebook } from 'react-icons/gr';
+import { BsTwitter } from 'react-icons/bs';
 
 function footer() {
   return (
-    <div>footer</div>
+    <div>
+     <i><b>
+       Recipe Sharing 
+     </b>
+      </i> 
+      &nbsp;
+      <BsInstagram/> &nbsp;
+      <GrFacebook/> &nbsp;
+      <BsTwitter/>
+    </div>
   )
 }
 
