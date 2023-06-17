1bf4382eae421c4263ee3752eb3f308a601efc7c
gyurmey <gyurmey1835@gmail.com>
2023-06-17 15:28:40 +0200

    footer

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -7,23 +7,17 @@ function header() {
     return (
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div style={{marginLeft: "40vw" }}>
-
-
         <a className="navbar-brand" style={{fontFamily: "initial", marginRight: "150px",marginLeft: "-300px"}} href="#">Recipe Sharing</a>
-
           <a className="navbar-brand" style={{fontFamily: "initial"}} href="#">Category</a>
           <a className="navbar-brand" style={{fontFamily: "initial"}}  href="#">Items</a> 
           <a className="navbar-brand" style={{fontFamily: "initial"}}  href="#">Recipe</a>
           <a className="navbar-brand" style={{fontFamily: "initial"}}  href="#">Check</a>
-
-
           <Link to="hi">
 				<Button id="names" className="btn mb-3" variant="outline-dark"
         style={{fontFamily: "initial", marginLeft: "250px",marginRight: "-300px", marginTop: "10px"}}  >
             Sign In
 				</Button>
 			</Link>
-
       </div>
     </nav>
 
