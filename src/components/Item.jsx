61e42162373bfc774321b4c5d9e9a403cf709ef2
gyurmey <gyurmey1835@gmail.com>
2023-06-25 15:36:41 +0200

    add comments

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import Stack from 'react-bootstrap/Stack';
 
 function Item({data}) {
   return (
   <div>
     <h1>
-      {data.description}
+      {data.author}
       </h1>
+      <Stack gap={3} style={{border: "ridge"}}>
+        {
+          data.comments?.length && data.comments.map(el => {
+            return <div className="p-2">
+              {  el.body}
+            </div>
+          })
+        }
+    </Stack>
   </div>
 )
 }
