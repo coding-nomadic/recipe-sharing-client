b7749ee044ef729351c645b884409a1458b03957
gyurmey <gyurmey1835@gmail.com>
2023-07-08 15:34:16 +0200

    header change

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -19,10 +19,10 @@ function Header({ logOut })
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div style={ { marginLeft: "40vw" } }>
         <a className="navbar-brand" style={ { fontFamily: "initial", marginRight: "150px", marginLeft: "-300px" } } href="#">Recipe Sharing</a>
-        <a className="navbar-brand" style={ { fontFamily: "initial" } } href="#">Category</a>
-        <a className="navbar-brand" style={ { fontFamily: "initial" } } href="#">Items</a>
-        <a className="navbar-brand" style={ { fontFamily: "initial" } } href="#">Recipe</a>
-        <a className="navbar-brand" style={ { fontFamily: "initial" } } href="#">Check</a>
+        <a className="navbar-brand" style={ { fontFamily: "initial" } } href="#">Dinners</a>
+        <a className="navbar-brand" style={ { fontFamily: "initial" } } href="#">Food News </a>
+        <a className="navbar-brand" style={ { fontFamily: "initial" } } href="#">Recipes</a>
+        <a className="navbar-brand" style={ { fontFamily: "initial" } } href="#">Tips</a>
         <Button id="names" className="btn mb-3" variant="outline-dark"
           style={ { fontFamily: "initial", marginLeft: "250px", marginRight: "-300px", marginTop: "10px" } }
           onClick={ signOut }
